93831033c337ce71ad89371ba02544f9f3fed839
niaanderson-po <niaanderson.po@gmail.com>
2025-03-22 14:01:22 -0400

    feat: add Linked List Standard Problem Set 1

---
+++ Linked List 1/standard_problem_set_2.py
@@ -0,0 +1,177 @@
+#Problem 1
+class Villager:
+    def __init__(self, name, species, catchphrase):
+        self.name = name
+        self.species = species
+        self.catchphrase = catchphrase
+        self.friends = []
+
+    def get_mutuals(self, new_contact):
+        result = []
+        for friend in self.friends:
+            if friend in new_contact.friends:
+                result.append(friend.name)
+        return result 
+
+bob = Villager("Bob", "Cat", "pthhhpth")
+marshal = Villager("Marshal", "Squirrel", "sulky")
+ankha = Villager("Ankha", "Cat", "me meow")
+fauna = Villager("Fauna", "Deer", "dearie")
+raymond = Villager("Raymond", "Cat", "crisp")
+stitches = Villager("Stitches", "Cub", "stuffin")
+
+bob.friends = [stitches, raymond, fauna]
+marshal.friends = [raymond, ankha, fauna]
+# print(bob.get_mutuals(marshal))
+
+ankha.friends = [marshal]
+# print(bob.get_mutuals(ankha))
+
+#Problem 2
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+kk_slider = Node("K.K. Slider")
+harriet = Node("Harriet")
+saharah = Node("Saharah")
+isabelle = Node("Isabelle")
+
+# Add code here to link the above nodes
+kk_slider.next = harriet
+harriet.next = saharah
+saharah.next = isabelle
+
+# print_linked_list(kk_slider)
+
+#Problem 3
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def add_first(head, task):
+    new_node = Node(task, head)
+    return new_node
+    
+
+task_1 = Node("shake tree")
+task_2 = Node("dig fossils")
+task_3 = Node("catch bugs")
+task_1.next = task_2
+task_2.next = task_3
+
+# Linked List: shake tree -> dig fossils -> catch bugs
+# print_linked_list(add_first(task_1, "check turnip prices"))
+
+#Problem 4
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def halve_list(head):
+    current_node = head
+    while current_node:
+        current_node.value = current_node.value/2
+        current_node = current_node.next
+    return head
+
+node_one = Node(5)
+node_two = Node(6)
+node_three = Node(7)
+node_one.next = node_two
+node_two.next = node_three
+
+# Input List: 5 -> 6 -> 7
+# print_linked_list(halve_list(node_one))
+
+#Problem 5 (*)
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def delete_tail(head):
+    if head is None or head.next is None:
+        return None
+    
+    current = head
+    while current.next and current.next.next:
+        current = current.next
+
+    current.next = None
+    return head
+    
+
+butterfly = Node("Common Butterfly")
+ladybug = Node("Ladybug")
+beetle = Node("Scarab Beetle")
+butterfly.next = ladybug
+ladybug.next = beetle
+
+# Input List: butterfly -> ladybug -> beetle
+# print_linked_list(delete_tail(butterfly))
+
+#Problem 6
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def find_min(head):
+    current = head
+    min = head.value
+
+    while current:
+        if current.value < min:
+            min = current.value
+        current = current.next
+    return min
+
+head1 = Node(5, Node(6, Node(7, Node(8))))
+head2 = Node(8, Node(5, Node(6, Node(7))))
+
+# Linked List: 5 -> 6 -> 7 -> 8
+print(find_min(head1))
+
+# Linked List: 8 -> 5 -> 6 -> 7
+print(find_min(head2))
+
+#Problem 7
